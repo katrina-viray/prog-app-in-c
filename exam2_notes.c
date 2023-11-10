064e88e531ba77960926b972ce33dbf7d2e56b05
doseka3 <katvir3@gmail.com>
2023-11-10 00:22:18 -0600

    ch 7 and w3 schools

--- exam2_notes.c
+++ exam2_notes.c
@@ -1,10 +1,156 @@
 // https://www.w3schools.com/c/c_structs.php
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
 
 /* Topics to look over******************************************************************* */
 /*
 1. Get really familiar with pointers.
 */
-/* H******************************************************************* */
+
+/* W3 School NOTES******************************************************************* */
+/* C Structures (structs)*************/
+/* Structs- way to grpup several related variables in one place.
+    Each variable in the struct is known as a "member" of the structure.
+    These can contain diff data types.*/
+struct MyStructure{ // declares a structure
+    int myNum; // member 1 (int var)
+    char myLetter; // member 2 (char var)
+    char myString[30];
+}; // remember to end structs with a semicolon!
+
+// now in main:
+struct MyStructure s1;
+s1.myNum = 13; // access members with the dot "." syntax
+s1.myLetter = 'a';
+printf("My number: %d", s1.myNum);
+
+// since strings can't be assigned like this:
+s1.myString = "some text";
+// we can work around this by using strcpy():
+strcpy(s1.myString, "some text");
+
+// can also assign variables when declaring the struct:
+struct MyStructure s2 = {13, 'a', "some text"};
+
+// can also assign one structure to another
+struct MyStructure s3;
+s3 = s1;
+
+/* C Enumeration (enum)*************/
+/* Enums- special type which reps a group of constants, AKA unchangeable values.*/
+enum Level{ // good practice is for the enum items to be uppercase
+    LOW, // 0
+    MEDIUM, // 1
+    HIGH = 75, // can change the values that each item holds.
+    EXTRA_HIGH // next items are updated accordingly, so now 76
+};
+
+// in main, to declare an enum variable:
+enum Level myVar = MEDIUM; // assigned values must be one of the items in the enum
+printf("%d", myVar); // outputs 1 since by default, items have values of 0, 1, 2, etc.
+
+
+/* TEXTBOOK NOTES******************************************************************* */
+
+/* Chapter 7: Pointers*************/
+/* Section 7.1: Pointer Variables***/
+// Review: In C, pointers hold the address to certain data types, like char* is a char pointer type.
+int x = 0;
+
+// while int* pt works too, use the one below since you can do int *pt1, *pt2, etc.
+// you cannot do this with int* pt (it would not work as intended).
+int *pt = &x; // int pointer holds the address of x, or points to x. 
+*pt = 3; // assigns value of 3 into x; dereference
+
+/* Section 7.2: Levels of Indirection***/
+// Levels of indirection- number of * attached to a variable to reference a value to which it points
+// Here, the cell named pt contains an andress of a cell which contains an address of a cell which contains an int. 
+int **pt; // pt is a pointer to a pointer to an int. 
+
+/* Section 7.3: Pointers and Arrays***/
+// Review: An array's name is a constant whose value is the address of the array's first element. 
+// Therefore, these statements both assign ptr the address of the first element of an array:
+float on_time[100];
+float* ptr;
+ptr = on_time;
+ptr = &on_time[0];
+
+// Here, the value of s2 cannot be changed since s2 is a pointer constant (to the first element in s2). 
+char s2[] = "blah blah";
+
+//Arrays and Pointer Artihmetic: 
+// Review: Can traverse arrays and access cells in a array through pointers, called pointer syntax.
+char letters[5];
+char *ptr = letters; // ptr points to address of letters[0]
+int count;
+
+// read in five characters into letters:
+for(count = 0; count < 5; count++){
+    *ptr = getchar(); // gets a character from the user
+    ++ptr;
+
+    //NOTE: these lines above can be combined to be: 
+    *ptr++ = getchar(); // * and increment operators have the same precedence
+}
+
+// printing characters from letters in reverse order
+for(count = 0; count < 5; count++){
+    --ptr;
+    putchar(*ptr); // outputs a char to output
+}
+
+// Pointer Operations:
+// Can also do ptr + 2, ptr - 2, ptr2 - ptr1, ptr1 >= ptr2
+// You CANNOT add two pointers, multiply 2 pointers, or divide one by another. 
+
+/* ptr2 - ptr1 is the cap btwn the cells pointed to by ptr1 and ptr2.
+if ptr2 and ptr1 point to the same cell, then it becomes zero. 
+if ptr2 - ptr1 is positive, then ptr1 points to a cell beyond the cell pointed to by ptr2
+if ptr2 - ptr1 is negative, then ptr1 points to a cell before the cell pointed to by ptr1.
+*/
+
+// These two statements both access the 13th element/cell in an array
+// publisher[12]
+// *(publisher + 12)
+
+/* Section 7.4: Pointers as Arguments to Functions***/
+// C does not support call by reference, where functions can receive a reference instead
+// copies of the arguments sent to it. C uses call by value exclusively, AKA where functions
+// work on copies of the argument sent to it instead of the OG argument itself. 
+
+/* By using pointer arguments, we can simulate call by reference. */
+int x = 999;
+f(&x); // simulates calling by reference
+
+void f(int *p){
+    *p += 2; // DOES change the value of x!
+    // return ++*p would return 999 + 1 = 1000. It derefs first and then ++ adds 1. 
+}
+
+// both headers for f would work due to arrays pointing to the first element :)
+int s[10];
+f(s);
+void f(int a[]); 
+void f(int *a);
+
+/* Chapter 7: Pointers*************/
+
+
+/* Chapter 7: Pointers*************/
+
+
+/* Chapter 7: Pointers*************/
+
+
+/* Chapter 7: Pointers*************/
+
+
+/* Chapter 7: Pointers*************/
+
+
+
+
 
 /* Program homework notes******************************************************************* */
 /* Audio Files********************************************/
@@ -47,3 +193,5 @@ stdin-
 gets- reads characters from the standard input until it encounters a newline
 fputs- writes a character string to the file referenced by its second argument
 */
+ return 0;
+}
--- hw10/Viray_Katrina_10.c
+++ hw10/Viray_Katrina_10.c
@@ -1,63 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int decode(char c)
-{
-    if((c >= 33) && (c <= 71))
-        return c;
-    else
-        return -1;
+#define SAMPLE_RATE 44100
+#define BITS_PER_SAMPLE 16
+
+void print_instructions_and_open_file(FILE **fp) {
+    char file_name[256];
+    printf("Enter the name of the output file: ");
+    scanf("%s", file_name);
+    *fp = fopen(file_name, "wb");
+    if (*fp == NULL) {
+        printf("Error: Unable to open the file.\n");
+        exit(1);
+    }
+}
+
+void fill_audio_data(short int **audio_data, int num_samples) {
+    int i, j;
+    for (i = 0; i < num_samples; i++) {
+        for (j = 0; j < 2; j++) {
+            audio_data[j][i] = 256*(rand()%256)+rand()%256-32768;
+        }
+    }
+}
+
+void write_wave_header(FILE *fp, int num_samples) {
+    int byte_rate = SAMPLE_RATE * 2 * BITS_PER_SAMPLE/8;
+    int block_align = 2 * BITS_PER_SAMPLE/8;
+    int data_size = num_samples * block_align;
+    int file_size = 44 + data_size - 8;
+    char chunk_id[4] = {'R', 'I', 'F', 'F'};
+    char format[4] = {'W', 'A', 'V', 'E'};
+    char sub_chunk1_id[4] = {'f', 'm', 't', ' '};
+    int sub_chunk1_size = 16;
+    short int audio_format = 1;
+    short int num_channels = 2;
+    int sample_rate = SAMPLE_RATE;
+    short int bits_per_sample = BITS_PER_SAMPLE;
+    char sub_chunk2_id[4] = {'d', 'a', 't', 'a'};
+
+    fwrite(chunk_id, sizeof(char), 4, fp);
+    fwrite(&file_size, sizeof(int), 1, fp);
+    fwrite(format, sizeof(char), 4, fp);
+    fwrite(sub_chunk1_id, sizeof(char), 4, fp);
+    fwrite(&sub_chunk1_size, sizeof(int), 1, fp);
+    fwrite(&audio_format, sizeof(short int), 1, fp);
+    fwrite(&num_channels, sizeof(short int), 1, fp);
+    fwrite(&sample_rate, sizeof(int), 1, fp);
+    fwrite(&byte_rate, sizeof(int), 1, fp);
+    fwrite(&block_align, sizeof(short int), 1, fp);
+    fwrite(&bits_per_sample, sizeof(short int), 1, fp);
+    fwrite(sub_chunk2_id, sizeof(char), 4, fp);
+    fwrite(&data_size, sizeof(int), 1, fp);
 }
 
 int main() {
-    FILE *fin, *fout;
+    int num_samples, i, j;
+    short int **audio_data;
+    FILE *fp;
+    float audio_length;
 
-    fin = fopen("input1.txt","r");
-    fout = fopen("output.txt","w");
+    print_instructions_and_open_file(&fp);
 
-    char c; // char to read in from file in
-    int num, f; // ASCII rep of the char, flag for switch statements
+    printf("Enter the length of the audio (in seconds): ");
+    scanf("%f", &audio_length);
 
-    while(fscanf(fin,"%c", &c) != EOF){
-        num = decode(c);
-        if(num!= -1){
-            if (c > 32 && c < 59){
-                f = 1;
-            }
-            else if (c > 58 && c < 69){
-                f = 2;
-            }
-            else if(c == 69){
-                f = 3;
-            }
-            else if(c == 70){
-                f = 4;
-            }
-            else if (c == 71){
-                f = 5;
-            }
-            switch(f){
-                case 1: // lowercase letters
-                    num = c + 64;
-                    fprintf(fout,"%c", num);
-                    break;
-                case 2: // numbers
-                    num = c - 11;
-                    fprintf(fout,"%c",num);
-                    break;
-                case 3: // blank space
-                    num = 32;
-                    fprintf(fout,"%c",num);
-                    break;
-                case 4: // period
-                    num = 46;
-                    fprintf(fout,"%c",num);
-                    break;
-                case 5: // newline
-                    fprintf(fout,"\n");
-                    break;
-            }
+    num_samples = (int)(audio_length * SAMPLE_RATE);
+    audio_data = (short int **)malloc(2 * sizeof(short int *));
+    for (i = 0; i < 2; i++) {
+        audio_data[i] = (short int *)malloc(num_samples * sizeof(short int));
+    }
+
+    srand(time(NULL));
+    fill_audio_data(audio_data, num_samples);
+
+    write_wave_header(fp, num_samples);
+
+    for (i = 0; i < num_samples; i++) {
+        for (j = 0; j < 2; j++) {
+            fwrite(&audio_data[j][i], sizeof(short int), 1, fp);
         }
     }
-    fclose(fin);
-    fclose(fout);
+
+    fclose(fp);
+
+    return 0;
 }
